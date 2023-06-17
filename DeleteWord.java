1729285231be053d05fd70b04d0b4220fe1bbe54
Arjun Singh <arjunsinghania913@gmail.com>
2023-06-17 16:55:28 +0530

    Dictionary

---
+++ DeleteWord.java
@@ -0,0 +1,46 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+public class DeleteWord extends JFrame
+{
+	private JTextField tfWord;
+	private JButton btnDelete;
+	
+	public DeleteWord()
+	{
+		super("Delete Word");
+	this.tfWord=new JTextField(20);
+	this.btnDelete=new JButton("Delete");
+	this.btnDelete.addActionListener(new ActionListener()
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if(DeleteWord.this.tfWord.getText().length()>0)
+			{
+			boolean done=Dictionary.deleteWord(DeleteWord.this.tfWord.getText());
+			if(!done){
+				 JOptionPane.showMessageDialog(DeleteWord.this,"Word Not Found.Please Try Again! ---> ","Delete Word",1);
+			}
+			else{
+				JOptionPane.showMessageDialog(DeleteWord.this,"Word Deleted Successfully! --->","Delete Word",1);
+			}}
+			else{
+				JOptionPane.showMessageDialog(DeleteWord.this,"Please Enter Word From Dictionary ! --->","Add Word",0);
+			}}
+		});
+		Container c=getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(new JLabel("Word To Delete"));
+		c.add(this.tfWord);
+		c.add(this.btnDelete);
+		pack();
+}}
+	
